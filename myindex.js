c709b4944068c7193b509d481c5ae179cc3ab53b
Ronczewski <Wojciech_Ronczewski@gmail.com>
2022-08-17 15:56:50 +0200

    init

---
+++ myindex.js
@@ -0,0 +1,5 @@
+/* instalujemy express-generator  -  npx express-generator --view=pug ./
+nastapnie prowadzi co dalej
+a - npm install
+b - w konsoli wpisujemy DEBUG=projekt-z-bartkiem:* 
+c - w konsoli wpisujemy DEBUG=projekt-z-bartkiem:* npm start    */
